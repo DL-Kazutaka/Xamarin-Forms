35a67afe9eb762a1a3c4e38db6224831f8c6a1a0
kazutaka.matsusaki <kazutaka.matsusaki@dartslive.com>
2014-12-05 18:12:54 +0900

    カメラアプリ用のプロジェクトを追加

---
+++ TakePicture/TakePicture/TakePicture/Views/CapturePage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakePicture.Views
+{
+    public partial class CapturePage
+    {
+        public CapturePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TakePicture/TakePicture/TakePicture/Views/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakePicture.Views
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnCaptureButtonClicked(object sender, EventArgs e)
+        {
+            this.Navigation.PushAsync(new CapturePage());
+        }
+    }
+}
